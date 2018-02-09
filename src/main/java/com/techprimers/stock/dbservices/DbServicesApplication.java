101154fd85e102763afa5b3678acf71e40bd1327
rawsharma <raw.sharma1379@gmail.com>
2018-02-09 07:32:41 -0600

    initial commit

---
+++ src/main/java/com/techprimers/stock/dbservices/DbServicesApplication.java
@@ -0,0 +1,12 @@
+package com.techprimers.stock.dbservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbServicesApplication.class, args);
+	}
+}
